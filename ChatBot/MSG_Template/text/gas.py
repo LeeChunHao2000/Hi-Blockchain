da8bb15b0ec77badc90550e7058206230ac2af22
Lee Chun Hao <meowinhere@gmail.com>
2020-09-22 21:46:11 +0800

    make it better

--- ChatBot/MSG_Template/text/gas.py
+++ ChatBot/MSG_Template/text/gas.py
@@ -8,9 +8,10 @@ def text_gas():
         data = requests.get('https://ethgasstation.info/api/ethgasAPI.json').json()
     except Exception as e:
         print ('Error! problem is {}'.format(e.args[0]))
+    safeLow, average, fast= data['safeLow']/10, data['average']/10, data['fast']/10
     return TextSendMessage(
             text = f'| Grenade Bot |\n\n'
-            f'慢速 (<30m) => {data['safeLow']/10}\n'
-            f'中等 (<5m) => {data['average']/10}\n'
-            f'快速 (<2m) => {data['fast']/10}\n\n'
+            f'慢速 (<30m) => {safeLow}\n'
+            f'中等 (<5m) => {average}\n'
+            f'快速 (<2m) => {fast}\n\n'
             f'單位: Gwei (Gas Price)，乙太坊當前手續費 (礦工費)\n\n')
